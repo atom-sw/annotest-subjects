79789b36f7c3cfc216462ecd19227b258c6e097b
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:39:59 +0200

    Finish tf_ThinPlateSpline_b23

---
+++ tf_ThinPlateSpline_b23/run_manual_failing_test.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# Exiting when any command fails
+set -e
+
+# Parameters
+BUG_ID="tf_ThinPlateSpline_b23"
+
+# Activate virtual environment
+source "$ANACONDA3_DIRECTORY"/etc/profile.d/conda.sh
+conda activate "$BUG_ID"
+
+# Run manual test
+python -m pip --version
+
+python -m pytest tests_manual/test_failing.py::test_failing
