72f9775f20eb31051760a5135806c22b11abefff
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:39:51 +0200

    Finish tf_ThinPlateSpline_b22

---
+++ tf_ThinPlateSpline_b22/make_env.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+# Exiting when any command fails
+set -e
+
+# Parameters
+BUG_ID="tf_ThinPlateSpline_b22"
+
+# Create virtual environment
+conda create -n "$BUG_ID" python=3.6 -y
+source "$ANACONDA3_DIRECTORY/etc/profile.d/conda.sh"
+conda activate "$BUG_ID"
+pip install --upgrade pip
+
+# Install requirements
+pip install -r requirements.txt
+pip install pytest
+
