f76c8a1a70dd24460254f35e8ca495deac6babbd
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:38:58 +0200

    Finish tf_ThinPlateSpline_b16

--- tf_ThinPlateSpline_b16/ThinPlateSpline2.py
+++ tf_ThinPlateSpline_b16/ThinPlateSpline2.py
@@ -103,8 +103,9 @@ def ThinPlateSpline2(U, source, target, out_size):
     py = tf.expand_dims(source[:,:,1], 2) # [bn, pn, 1]
     d2 = tf.square(x_t_flat - px) + tf.square(y_t_flat - py)
     r = d2 * tf.log(d2 + 1e-6) # [bn, pn, h*w]
-    x_t_flat_g = tf.tile(x_t_flat, tf.pack([num_batch, 1, 1])) # [bn, 1, h*w]
-    y_t_flat_g = tf.tile(y_t_flat, tf.pack([num_batch, 1, 1])) # [bn, 1, h*w]
+    # x_t_flat_g = tf.tile(x_t_flat, tf.pack([num_batch, 1, 1])) # [bn, 1, h*w]  # repo_change
+    x_t_flat_g = tf.tile(x_t_flat, tf.stack([num_batch, 1, 1])) # [bn, 1, h*w]  # repo_change
+    y_t_flat_g = tf.tile(y_t_flat, tf.pack([num_batch, 1, 1])) # [bn, 1, h*w]  # repo_bug
     ones = tf.ones_like(x_t_flat_g) # [bn, 1, h*w]
 
     grid = tf.concat(1, [ones, x_t_flat_g, y_t_flat_g, r]) # [bn, 3+pn, h*w]
@@ -144,7 +145,8 @@ def ThinPlateSpline2(U, source, target, out_size):
     num_point  = tf.shape(source)[1]
     
     ones = tf.ones([num_batch, num_point, 1], dtype="float32")
-    p = tf.concat(2, [ones, source]) # [bn, pn, 3]
+    # p = tf.concat(2, [ones, source]) # [bn, pn, 3]  # repo_change
+    p = tf.concat([ones, source], 2) # [bn, pn, 3]  # repo_change
 
     p_1 = tf.reshape(p, [num_batch, -1, 1, 3]) # [bn, pn, 1, 3]
     p_2 = tf.reshape(p, [num_batch, 1, -1, 3]) # [bn, 1, pn, 3]
@@ -152,15 +154,19 @@ def ThinPlateSpline2(U, source, target, out_size):
     r = d2 * tf.log(d2 + 1e-6) # [bn, pn, pn]
 
     zeros = tf.zeros([num_batch, 3, 3], dtype="float32")
-    W_0 = tf.concat(2, [p, r]) # [bn, pn, 3+pn]
-    W_1 = tf.concat(2, [
-      zeros, tf.transpose(p, [0, 2, 1])]) # [bn, 3, pn+3]
-    W = tf.concat(1, [W_0, W_1]) # [bn, pn+3, pn+3]
+    # W_0 = tf.concat(2, [p, r]) # [bn, pn, 3+pn]  # repo_change
+    W_0 = tf.concat([p, r], 2) # [bn, pn, 3+pn]  # repo_change
+    # W_1 = tf.concat(2, [  # repo_change
+      # zeros, tf.transpose(p, [0, 2, 1])]) # [bn, 3, pn+3]  # repo_change
+    W_1 = tf.concat([zeros, tf.transpose(p, [0, 2, 1])], 2) # [bn, 3, pn+3]  # repo_change
+    # W = tf.concat(1, [W_0, W_1]) # [bn, pn+3, pn+3]  # repo_change
+    W = tf.concat([W_0, W_1], 1) # [bn, pn+3, pn+3]  # repo_change
     W_inv = tf.matrix_inverse(W) 
 
     tp = tf.pad(target, 
       [[0, 0], [0, 3], [0, 0]], "CONSTANT") # [bn, pn+3, 2]
-    T = tf.batch_matmul(W_inv, tp) # [bn, pn+3, 2]
+    # T = tf.batch_matmul(W_inv, tp) # [bn, pn+3, 2]  # repo_change
+    T = tf.matmul(W_inv, tp) # [bn, pn+3, 2]  # repo_change
     T = tf.transpose(T, [0, 2, 1]) # [bn, 2, pn+3]
 
     return T
