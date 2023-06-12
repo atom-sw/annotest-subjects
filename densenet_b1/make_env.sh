9cfe9583b0f91030285e31a6c2826f3b5bbea314
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 16:59:40 +0200

    Update densenet_b1

--- densenet_b1/make_env.sh
+++ densenet_b1/make_env.sh
@@ -3,11 +3,6 @@
 # Exiting when any command fails
 set -e
 
-<<<<<<< HEAD
-# Create virtual environment
-python3.6 -m venv env
-source env/bin/activate
-=======
 # Parameters
 BUG_ID="densenet_b1"
 
@@ -15,7 +10,6 @@ BUG_ID="densenet_b1"
 conda create -n "$BUG_ID" python=3.6 -y
 source "$ANACONDA3_DIRECTORY/etc/profile.d/conda.sh"
 conda activate "$BUG_ID"
->>>>>>> dev
 pip install --upgrade pip
 
 # Install requirements
@@ -23,12 +17,8 @@ pip install -r requirements.txt
 pip install pytest
 
 # Remove keras.json file from home
-<<<<<<< HEAD
-rm ~/.keras/keras.json
-=======
 KERAS_CONF_JSON=~/.keras/keras.json
 if [[ -f "$KERAS_CONF_JSON" ]]; then
     rm "$KERAS_CONF_JSON"
 fi
 
->>>>>>> dev
