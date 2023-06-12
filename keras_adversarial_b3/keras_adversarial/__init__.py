e9a0dcd0d0913f87173a3e60d96e90ae46c7c5e3
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:32:44 +0200

    Finish keras_adversarial_b3

--- keras_adversarial_b3/keras_adversarial/__init__.py
+++ keras_adversarial_b3/keras_adversarial/__init__.py
@@ -1,6 +1,13 @@
-from adversarial_utils import gan_targets, build_gan, normal_latent_sampling, eliminate_z, fix_names, simple_gan
-from adversarial_utils import n_choice, simple_bigan, gan_targets_hinge
-from adversarial_model import AdversarialModel
-from adversarial_optimizers import AdversarialOptimizerSimultaneous, AdversarialOptimizer
-from adversarial_optimizers import AdversarialOptimizerAlternating
-from image_grid_callback import ImageGridCallback
+# from adversarial_utils import gan_targets, build_gan, normal_latent_sampling, eliminate_z, fix_names, simple_gan  # repo_change
+# from adversarial_utils import n_choice, simple_bigan, gan_targets_hinge  # repo_change
+# from adversarial_model import AdversarialModel  # repo_change
+# from adversarial_optimizers import AdversarialOptimizerSimultaneous, AdversarialOptimizer  # repo_change
+# from adversarial_optimizers import AdversarialOptimizerAlternating  # repo_change
+# from image_grid_callback import ImageGridCallback  # repo_change
+
+from .adversarial_utils import gan_targets, build_gan, normal_latent_sampling, eliminate_z, fix_names, simple_gan  # repo_change
+from .adversarial_utils import n_choice, simple_bigan, gan_targets_hinge  # repo_change
+from .adversarial_model import AdversarialModel  # repo_change
+from .adversarial_optimizers import AdversarialOptimizerSimultaneous, AdversarialOptimizer  # repo_change
+from .adversarial_optimizers import AdversarialOptimizerAlternating  # repo_change
+from .image_grid_callback import ImageGridCallback  # repo_change
