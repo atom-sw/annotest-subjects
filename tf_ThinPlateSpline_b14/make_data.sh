0b8380cdc69679e137974ed10e8853585fb6f1d7
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:38:43 +0200

    Finish tf_ThinPlateSpline_b14

---
+++ tf_ThinPlateSpline_b14/make_data.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+# Exiting when any command fails
+set -e
+
+# Parameters
+BUG_ID="tf_ThinPlateSpline_b14"
+DATA_DIRECTORY_PATH=~/annotest_subjects_data
+
+# File id on Google drive
+FILE_ID=1dJfPd8VyHlbwxno3vY67JheXG2x-whgG
+
+BUG_ID_DATA_DIRECTORY_PATH=$DATA_DIRECTORY_PATH/$BUG_ID
+FILE_PATH=$BUG_ID_DATA_DIRECTORY_PATH/$BUG_ID.zip
+
+if [ -d "$BUG_ID_DATA_DIRECTORY_PATH" ]; then
+  rm -rf $BUG_ID_DATA_DIRECTORY_PATH
+fi
+
+mkdir -p $BUG_ID_DATA_DIRECTORY_PATH
+
+wget --load-cookies /tmp/cookies.txt \
+ "https://docs.google.com/uc?export=download&confirm=$(wget \
+  --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
+   --no-check-certificate \
+   'https://docs.google.com/uc?export=download&id=$FILE_ID' -O- \
+    | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" \
+    -O $FILE_PATH && rm -rf /tmp/cookies.txt
+
+unzip $FILE_PATH -d $BUG_ID_DATA_DIRECTORY_PATH
+rm $FILE_PATH
