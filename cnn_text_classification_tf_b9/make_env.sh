506cb763e2dc7cab591bc55a1e284890289816a1
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:28:11 +0200

    Finish cnn_text_classification_tf_b9

---
+++ cnn_text_classification_tf_b9/make_env.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+# Exiting when any command fails
+set -e
+
+# Parameters
+BUG_ID="cnn_text_classification_tf_b9"
+
+# Create virtual environment
+conda create -n "$BUG_ID" python=3.6 -y
+source "$ANACONDA3_DIRECTORY/etc/profile.d/conda.sh"
+conda activate "$BUG_ID"
+pip install --upgrade pip
+
+# Install requirements
+pip install -r requirements.txt
+pip install pytest
+
