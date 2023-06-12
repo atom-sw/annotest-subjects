2f1420a3fc86210f2771111208cfae828279f8b8
Moe <rezaalipour.mohammad@gmail.com>
2023-06-12 15:33:53 +0200

    Finish keras_image_segmentation_b1

---
+++ keras_image_segmentation_b1/make_env.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# Exiting when any command fails
+set -e
+
+# Parameters
+BUG_ID="keras_image_segmentation_b1"
+
+# Create virtual environment
+conda create -n "$BUG_ID" python=3.6 -y
+source "$ANACONDA3_DIRECTORY/etc/profile.d/conda.sh"
+conda activate "$BUG_ID"
+pip install --upgrade pip
+
+# Install requirements
+pip install -r requirements.txt
+pip install pytest
+
+# Remove keras.json file from home
+KERAS_CONF_JSON=~/.keras/keras.json
+if [[ -f "$KERAS_CONF_JSON" ]]; then
+    rm "$KERAS_CONF_JSON"
+fi
