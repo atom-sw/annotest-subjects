bb19a1036fc9f683c1e349faf88e91f52ff44889
Moe <rezaalipour.mohammad@gmail.com>
2023-06-23 18:07:53 +0200

    Finish TF_PG_GANS_b2

--- TF_PG_GANS_b2/Tensorflow_progressive_growing_of_gans/model.py
+++ TF_PG_GANS_b2/Tensorflow_progressive_growing_of_gans/model.py
@@ -8,7 +8,10 @@ from keras import activations
 from keras import initializers
 from keras.models import Model
 import numpy as np
+# from layers import *  # repo_change
 from .layers import *  # repo_change
+
+from annotest import an_language as an
 
 linear, linear_init = activations.linear,       initializers.VarianceScaling(scale=1.0, mode='fan_in', distribution='normal')
 relu,   relu_init = activations.relu,         initializers.he_normal()
@@ -118,6 +121,18 @@ def Generator(num_channels=1,
     model = Model(inputs=inputs, outputs=[output])
 
 
+@an.arg("num_channels", an.integers(min_value=1))
+@an.arg("resolution", an.sampled([4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]))
+@an.arg("label_size", an.integers(max_value=10))
+@an.arg("fmap_base", an.sampled([4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]))
+@an.arg("fmap_decay", an.floats(min_value=0, max_value=1, exclude_min=True))
+@an.arg("fmap_max", an.sampled([4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]))
+@an.arg("mbstat_func", an.sampled(['Tstdeps']))
+@an.arg("mbstat_avg", an.sampled([None, 'all', 'flat', 'spatial', 'none', 'gpool']))
+@an.arg("mbdisc_kernels", an.multiple(an.integers(), None))
+@an.arg("use_wscale", an.sampled([True, False]))
+@an.arg("use_gdrop", an.sampled([True, False]))
+@an.arg("use_layernorm", an.sampled([True, False]))
 def Discriminator(num_channels=1,        # Overridden based on dataset.
         resolution=32,       # Overridden based on dataset.
         label_size=0,        # Overridden based on dataset.
@@ -151,10 +166,8 @@ def Discriminator(num_channels=1,        # Overridden based on dataset.
             actv,
             init,
             name=None):
-        # layer = Conv2D(num_channels, 1, activation=actv,  # repo_change
-        #                kernel_initializer=init, pad='same', name=name + 'NIN')  # repo_change
-        layer = Conv2D(num_channels, 1, activation=actv,  # repo_change
-                       kernel_initializer=init, padding='same', name=name + 'NIN')  # repo_change
+        layer = Conv2D(num_channels, 1, activation=actv,
+                       kernel_initializer=init, pad='same', name=name + 'NIN')  # repo_bug
         net = layer(net)
         if use_wscale:
             layer = WScaleLayer(layer, name=name + 'NINWS')
@@ -183,8 +196,7 @@ def Discriminator(num_channels=1,        # Overridden based on dataset.
             net = layer(net)
         return net
 
-    # inputs = Input(shape=[None, 2**R, 2**R,num_channels], name='Dimages')  # repo_change
-    inputs = Input(shape=[2 ** R, 2 ** R, num_channels], name='Dimages')  # repo_change
+    inputs = Input(shape=[None, 2**R, 2**R,num_channels], name='Dimages')
     net = NINBlock(inputs, numf(R-1), lrelu, lrelu_init, name='D%dx' % (R-1))
     for i in range(R-1, 1, -1):
         net = ConvBlock(net, numf(i), 3, lrelu, lrelu_init, 1, name='D%db' % i)
