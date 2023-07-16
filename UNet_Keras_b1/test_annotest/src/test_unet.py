deca9f08ff043fa3facbd3501ed3051d57ffd63a
Moe <rezaalipour.mohammad@gmail.com>
2023-07-16 16:17:21 +0200

    Change bug revealing test in UNet_Keras_b1

--- UNet_Keras_b1/test_annotest/src/test_unet.py
+++ UNet_Keras_b1/test_annotest/src/test_unet.py
@@ -1,7 +1,7 @@
 import unittest
 import hypothesis as hy
 import hypothesis.strategies as st
-from src.unet import UNet, generator_UNet__add_encode_layers_input_layer
+from src.unet import UNet
 
 
 class Test_UNet(unittest.TestCase):
@@ -10,14 +10,6 @@ class Test_UNet(unittest.TestCase):
     @hy.settings(deadline=None)
     def test_init(self, noArgCall):
         UNet()
-
-    @hy.given(filters=st.sampled_from([32, 64, 128, 256, 512]), st_for_data
-        =st.data())
-    @hy.settings(deadline=None)
-    def test___add_Encode_layers(self, filters, st_for_data):
-        obj = UNet()
-        inputLayer = generator_UNet__add_encode_layers_input_layer()
-        obj.__add_Encode_layers(filters, inputLayer)
 
     @hy.given(st_for_data=st.data())
     @hy.settings(deadline=None)
--- UNet_Keras_b1/tests_manual/test_failing.py
+++ UNet_Keras_b1/tests_manual/test_failing.py
@@ -1,10 +1,5 @@
-from keras import Input
-
 from src.unet import UNet
 
 
 def test_failing():
-    filters = 32
-    obj = UNet()
-    inputLayer = Input((258, 258, 3))
-    obj.__add_Encode_layers(filters, inputLayer)
+    UNet()
